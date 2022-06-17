7a06d0f84d7859b3dd4a9dac5023b53fff6285ad
akshayb407 <akshayb407@gmail.com>
2022-06-18 01:06:46 +0530

    UC3 added

--- SnakeLadder/SnakeLadder/Program.cs
+++ SnakeLadder/SnakeLadder/Program.cs
@@ -6,19 +6,39 @@ namespace SnakeLadder
     {
         static void Main(string[] args)
         {
-            {
-                Console.WriteLine("Snake and Ladder game played with single player at start position 0");
-                int position = 0;
-                Console.WriteLine("Position is " + position);
+            Console.WriteLine("Snake and Ladder game played with single player at start position 0");
+            int position = 0;
+            Console.WriteLine("Position is " + position);
 
-                Random random = new Random();
-                int number = random.Next(0, 7);
-                Console.WriteLine(number);
+            Random random = new Random();
+            int number = random.Next(0, 7);
+            Console.WriteLine(number);
+
+            int option = random.Next(0, 3);
+
+            switch (option)
+            {
+                case 0:
+                    Console.WriteLine("No play");
+                    break;
+                case 1:
+                    Console.WriteLine("Ladder");
+                    position = position + number;
+                    break;
+                case 2:
+                    Console.WriteLine("Snake");
+                    position = position - number;
+                    break;
+
+
             }
         }
     }
-
 }
+
+
+
+
 
 
 
