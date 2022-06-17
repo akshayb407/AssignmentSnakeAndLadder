2f04b09d545d1991a5c1efe81271a5e38b3948af
akshayb407 <akshayb407@gmail.com>
2022-06-18 01:01:23 +0530

    UC2 added

--- SnakeLadder/SnakeLadder/Program.cs
+++ SnakeLadder/SnakeLadder/Program.cs
@@ -10,9 +10,16 @@ namespace SnakeLadder
                 Console.WriteLine("Snake and Ladder game played with single player at start position 0");
                 int position = 0;
                 Console.WriteLine("Position is " + position);
+
+                Random random = new Random();
+                int number = random.Next(0, 7);
+                Console.WriteLine(number);
             }
         }
     }
+
 }
-    
+
+
+
 
