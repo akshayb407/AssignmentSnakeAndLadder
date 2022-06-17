cd460d4892703ad32145b19bd00f319a4a8ef53b
akshayb407 <akshayb407@gmail.com>
2022-06-18 01:29:14 +0530

    UC6 added

--- SnakeLadder/SnakeLadder/Program.cs
+++ SnakeLadder/SnakeLadder/Program.cs
@@ -9,12 +9,15 @@ namespace SnakeLadder
             Console.WriteLine("Snake and Ladder game played with single player at start position 0");
             int position = 0;
             Console.WriteLine("Position is " + position);
-
+            int NoOfDieroll = 0;
             Random random = new Random();
             int number = random.Next(0, 7);
+            NoOfDieroll++;
+            Console.WriteLine("Number of die rolls " + NoOfDieroll);
             Console.WriteLine(number);
 
             int option = random.Next(0, 3);
+
 
             while (position <= 100)
             {
@@ -22,18 +25,23 @@ namespace SnakeLadder
                 {
                     case 0:
                         Console.WriteLine("No play");
+                        Console.WriteLine("the position is" + position);
                         break;
                     case 1:
                         Console.WriteLine("Ladder");
                         position = position + number;
+
                         if (position > 100)
                         {
                             position = position - number;
+
                         }
+                        Console.WriteLine("the position is" + position);
                         break;
                     case 2:
                         Console.WriteLine("Snake");
                         position = position - number;
+                        Console.WriteLine("the position is" + position);
                         break;
 
 
@@ -41,6 +49,7 @@ namespace SnakeLadder
                 if (position < 0)
                 {
                     position = 0;
+                    Console.WriteLine("the position is" + position);
                 }
             }
         }
